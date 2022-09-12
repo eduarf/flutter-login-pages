48e8f6c71bca4fdc11cce0b3c70b02d044b05d17
eduarf <fraude_f@yahoo.com>
2022-09-12 16:20:09 +0300

    prefixIcon added

--- lib/login_two/screens/login_two.dart
+++ lib/login_two/screens/login_two.dart
@@ -11,15 +11,15 @@ class LoginPageTwo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final linearBoxDecoration = BoxDecoration(
-                  gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    AppTwoColors().mainColor,
-                    AppTwoColors().linearColorTwo,
-                  ],
-                  ),
-                );
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          AppTwoColors().mainColor,
+          AppTwoColors().linearColorTwo,
+        ],
+      ),
+    );
     return Scaffold(
       backgroundColor: AppTwoColors().white,
       resizeToAvoidBottomInset: false,
@@ -51,19 +51,37 @@ class _BotSide extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Text(AppStrings().needAccount,style: AppTextStyles().normalStyle.copyWith(color: Colors.black87),),
+        Text(
+          AppStrings().needAccount,
+          style: AppTextStyles().normalStyle.copyWith(color: Colors.black87),
+        ),
         SizedBox(
           width: MediaQuery.of(context).size.width * .7,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              SvgPicture.asset('assets/google.svg', height: 35, width: 35,),
-              SvgPicture.asset('assets/facebook.svg',height: 35, width: 35,),
-              SvgPicture.asset('assets/twitter.svg', height: 35, width: 35,),
+              SvgPicture.asset(
+                'assets/google.svg',
+                height: 35,
+                width: 35,
+              ),
+              SvgPicture.asset(
+                'assets/facebook.svg',
+                height: 35,
+                width: 35,
+              ),
+              SvgPicture.asset(
+                'assets/twitter.svg',
+                height: 35,
+                width: 35,
+              ),
             ],
           ),
         ),
-        Button(buttonText: AppStrings().signUp, width: MediaQuery.of(context).size.width * .5,),
+        Button(
+          buttonText: AppStrings().signUp,
+          width: MediaQuery.of(context).size.width * .5,
+        ),
       ],
     );
   }
@@ -86,18 +104,34 @@ class _TopSide extends StatelessWidget {
         children: [
           Text(
             AppStrings().login,
-            style: AppTextStyles().bigTitle.copyWith(color: AppTwoColors().white),
-          ),
-           Padding(
-            padding: AppTwoPaddings().horizontal25,
-            child: InputWidget(hintText: AppStrings().hintTextMail,),
+            style:
+                AppTextStyles().bigTitle.copyWith(color: AppTwoColors().white),
           ),
           Padding(
             padding: AppTwoPaddings().horizontal25,
-            child: InputWidget(hintText: AppStrings().hintTextMail, obscureText: true,),
+            child: InputWidget(
+              hintText: AppStrings().hintTextMail,
+              prefixIcon: Icon(
+                Icons.email,
+                color: AppTwoColors().white,
+              ),
+            ),
           ),
-          Button(buttonText: AppStrings().login, width: MediaQuery.of(context).size.width * .9,),
-          Text(AppStrings().forgotPassword ,style: AppTextStyles().normalStyle,),
+          Padding(
+            padding: AppTwoPaddings().horizontal25,
+            child: InputWidget(
+                hintText: AppStrings().hinTextPassword,
+                obscureText: true,
+                prefixIcon: Icon(Icons.lock, color: AppTwoColors().white)),
+          ),
+          Button(
+            buttonText: AppStrings().login,
+            width: MediaQuery.of(context).size.width * .9,
+          ),
+          Text(
+            AppStrings().forgotPassword,
+            style: AppTextStyles().normalStyle,
+          ),
         ],
       ),
     );
@@ -121,9 +155,13 @@ class Button extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 18),
       decoration: BoxDecoration(
         color: AppTwoColors().mainColorDark,
-        borderRadius: BorderRadius.circular(18), 
+        borderRadius: BorderRadius.circular(18),
       ),
-      child: Text(buttonText ,style: AppTextStyles().buttonTextStyle, textAlign: TextAlign.center,),
+      child: Text(
+        buttonText,
+        style: AppTextStyles().buttonTextStyle,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
@@ -133,25 +171,28 @@ class InputWidget extends StatelessWidget {
     Key? key,
     required this.hintText,
     this.obscureText = false,
+    required this.prefixIcon,
   }) : super(key: key);
 
   final String hintText;
   final bool obscureText;
+  final Icon prefixIcon;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       obscureText: obscureText,
-      style: TextStyle(color: AppTwoColors().white),
+      style: TextStyle(color: AppTwoColors().white,),
       decoration: InputDecoration(
+        prefixIcon: prefixIcon,
         hintText: hintText,
         hintStyle: TextStyle(color: AppTwoColors().white),
         focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: AppTwoColors().white, width: 1.0),
-            ),
-            enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: AppTwoColors().white, width: 1.0),
-            ),
+          borderSide: BorderSide(color: AppTwoColors().white, width: 1.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: AppTwoColors().white, width: 1.0),
+        ),
       ),
     );
   }
